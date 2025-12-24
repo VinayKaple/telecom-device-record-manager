bae8c5d87b689b84be6291efb51523d9498dae22
VinayKaple <vkaple1@stevens.edu>
2025-12-24 10:47:51 +0100

    first commit, spring boot app connected to db container

---
+++ telecom-device-manager/src/main/java/com/example/telecomdevicemanager/TelecomDeviceManagerApplication.java
@@ -0,0 +1,14 @@
+package com.example.telecomdevicemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomDeviceManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelecomDeviceManagerApplication.class, args);
+        System.out.println("Hello from TelecomDeviceManagerApplication!!");
+    }
+
+}
---
+++ telecom-device-manager/src/test/java/com/example/telecomdevicemanager/TelecomDeviceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telecomdevicemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomDeviceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
